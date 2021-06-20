3c7141eac53f3cc9e0fc2c69f7e26df5d77faff5
vikash chandola <vikashchandola26@gmail.com>
2021-06-20 14:52:51 +0530

    Updates log.hpp to handle on argument
    variable arguments macros can't be called with no arguments. There
    must always be at least one argument. This change will allow log
    messages with no arguemnts.

--- include/log.hpp
+++ include/log.hpp
@@ -1,10 +1,12 @@
 #include <syslog.h>
 
-#define DEBUG(fmt, ...)  syslog(LOG_DEBUG, "[DEBUG] " fmt, ##__VA_ARGS__)
-#define INFO(fmt, ...)   syslog(LOG_INFO, "[INFO] " fmt, ##__VA_ARGS__)
-#define NOTICE(fmt, ...) syslog(LOG_NOTICE, "[NOTICE] " fmt, ##__VA_ARGS__)
-#define WARN(fmt, ...)   syslog(LOG_WARNING, "[WARNING] " fmt, ##__VA_ARGS__)
-#define ERROR(fmt, ...)  syslog(LOG_ERR, "[ERROR] " fmt, ##__VA_ARGS__)
+#define DEBUG(...)  syslog(LOG_DEBUG, ##__VA_ARGS__)
+#define INFO(...)   syslog(LOG_INFO, ##__VA_ARGS__)
+#define NOTICE(...) syslog(LOG_NOTICE, ##__VA_ARGS__)
+#define WARN(...)   syslog(LOG_WARNING, ##__VA_ARGS__)
+#define ERROR(...)  syslog(LOG_ERR, ##__VA_ARGS__)
+
+#define mark        DEBUG("M [%s][%d]", __func__, __LINE__)
 
 /*
 int main() {
