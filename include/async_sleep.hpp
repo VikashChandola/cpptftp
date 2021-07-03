c348d08d6d914c48b6dc954725164e2ab2ed31de
vikash chandola <vikashchandola26@gmail.com>
2021-07-03 15:12:31 +0530

    Adds async sleep class.
    Added a time generator class. This will be used for creating random
    delays between frames

---
+++ include/async_sleep.hpp
@@ -0,0 +1,47 @@
+#ifndef ___ASYNC_SLEEP_HPP__
+#define ___ASYNC_SLEEP_HPP__
+#include <boost/asio.hpp>
+#include <chrono>
+#include <functional>
+#include <random>
+
+typedef std::chrono::duration<uint64_t, std::milli> ms_duration;
+
+class duration_generator {
+private:
+protected:
+public:
+  virtual ms_duration operator()() = 0;
+  virtual ~duration_generator(){};
+};
+
+class constant_duration_generator : public duration_generator {
+private:
+  ms_duration delay;
+
+public:
+  constant_duration_generator(const ms_duration &_delay) : delay(_delay) {}
+  ms_duration operator()() override { return delay; }
+};
+
+class random_duration_generator : public duration_generator {
+private:
+  ms_duration lower_limit;
+  ms_duration upper_limit;
+  std::random_device rd;
+  std::mt19937 gen;
+  std::uniform_int_distribution<> distrib;
+
+public:
+  random_duration_generator(ms_duration lower_limit, ms_duration upper_limit)
+      : gen(rd()),
+        distrib(lower_limit.count(), upper_limit.count()) {}
+
+  random_duration_generator(const random_duration_generator &original)
+      : gen(rd()),
+        distrib(original.lower_limit.count(), original.upper_limit.count()) {}
+
+  ms_duration operator()() { return ms_duration(distrib(rd)); }
+};
+
+#endif
---
+++ test/async_sleep_test.cpp
@@ -0,0 +1,47 @@
+#define BOOST_TEST_MODULE async sleep
+#include <boost/test/data/monomorphic.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/included/unit_test.hpp>
+#include <unordered_map>
+
+#include "async_sleep.hpp"
+
+namespace bdata = boost::unit_test::data;
+
+BOOST_DATA_TEST_CASE(constant_sleep_duration_test, bdata::make({1, 2, 3, 4, 5}), sample) {
+  ms_duration second(sample);
+  constant_duration_generator cdg(second);
+  duration_generator &gen = cdg;
+
+  // Geneartor must always return same duration
+  BOOST_TEST(gen().count() == sample);
+  BOOST_TEST(gen().count() == sample);
+  BOOST_TEST(gen().count() == sample);
+  BOOST_TEST(gen().count() == sample);
+}
+
+BOOST_TEST_DONT_PRINT_LOG_VALUE(ms_duration)
+BOOST_DATA_TEST_CASE(random_sleep_duration_test,
+                     bdata::make({ms_duration(1000)}) ^ bdata::make({ms_duration(2000)}),
+                     lower,
+                     upper) {
+  const uint32_t invokation_count = 1000;
+  class ms_duration_hasher {
+  public:
+    std::size_t operator()(const ms_duration &key) const noexcept { return key.count(); }
+  };
+
+  std::unordered_map<ms_duration, uint32_t, ms_duration_hasher> map;
+
+  random_duration_generator rdg(lower, upper);
+  duration_generator &gen = rdg;
+
+  for (uint32_t i = 0; i < invokation_count; i++) {
+    ms_duration duration = gen();
+    // duration must be in between requested range
+    BOOST_TEST((duration <= upper && duration >= lower));
+    map[duration] += 1;
+  }
+  // To check that there are limited repeatation. In case generator start to return same duration.
+  BOOST_TEST(map.size() > invokation_count / 10);
+}
