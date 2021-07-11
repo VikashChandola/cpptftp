13ead80491dc9c11f57fa459ba20fae7ae25eb36
vikash chandola <vikashchandola26@gmail.com>
2021-07-11 20:41:32 +0530

    Adds extensive logging option for debugging

--- include/log.hpp
+++ include/log.hpp
@@ -1,10 +1,19 @@
 #include <syslog.h>
+#include "project_config.hpp"
 
 #define DEBUG(...)  syslog(LOG_DEBUG, ##__VA_ARGS__)
 #define INFO(...)   syslog(LOG_INFO, ##__VA_ARGS__)
 #define NOTICE(...) syslog(LOG_NOTICE, ##__VA_ARGS__)
 #define WARN(...)   syslog(LOG_WARNING, ##__VA_ARGS__)
 #define ERROR(...)  syslog(LOG_ERR, ##__VA_ARGS__)
+
+
+
+#ifdef CONF_EXTENSIVE_DEBUG_LOG
+#define XDEBUG(...) syslog(LOG_DEBUG, ##__VA_ARGS__)
+#else
+#define XDEBUG(...)
+#endif
 
 #define mark        DEBUG("M [%s][%d]", __func__, __LINE__)
 
