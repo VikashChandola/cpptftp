433f25aafd9d3b14aee768c4c46be8eeff10c5fb
vikash chandola <vikashchandola26@gmail.com>
2021-07-17 12:21:08 +0530

    Adds file_io reader and UT for reader

--- include/file_io.hpp
+++ include/file_io.hpp
@@ -1,42 +1,39 @@
 #ifndef __FILE_IO_HPP__
 #define __FILE_IO_HPP__
 #include <fstream>
+#include <memory>
 
 namespace fileio {
 
 class reader {
   std::fstream handle;
-  std::unique_ptr<char[]> buffer;
+
 public:
-  base(const std::string &filename, uint16_t buffer_size = 512)
-    : handle(filename, std::ios::in | std::ios::binary),
-      buffer(new char[buffer_size])
-  {
-  }
+  reader(const std::string &filename) : handle(filename, std::ios::in | std::ios::binary) {}
 
-  bool is_open(){
-    return this->handle.is_open();
-  }
+  bool is_open() { return this->handle.is_open(); }
 
-  /* Reads from disk and start filling from itr_begin till itr_end. If end of file hit before
-   * reaching itr_end then is_full will be set to false, otherwise is_full will be set to true.
-   * Returns true if read operation was successfull otherwise return false
+  /* Reads from disk and start filling from itr_begin till itr_end. Returns true if read went through without
+   * any problem. bytes_read argument is set to number of bytes read from the file
    */
-  template<typename T>
-  bool fill_buffer(T itr_begin, T itr_end, bool &is_full ) noexcept {
-    if(!this->is_open()){
+  template <typename T>
+  bool fill_buffer(T itr_begin, T itr_end, std::streamsize &bytes_read) noexcept {
+    size_t buffer_size = itr_end - itr_begin;
+    std::unique_ptr<char[]> buffer(new char[buffer_size]);
+    if (!this->is_open()) {
       return false;
     }
-    try{
-      this->read_stream.read(this->buffer.get(), itr_end - itr_begin);
-    } catch (const std::ios_base::failure& e){
+    try {
+      this->handle.read(buffer.get(), buffer_size);
+      bytes_read = this->handle.gcount();
+    } catch (const std::ios_base::failure &e) {
       this->handle.close();
       return false;
     }
-    while(itr_begin != itr_end){
-      *itr_begin = 
-    }
+    std::copy(&buffer.get()[0], &buffer.get()[0] + buffer_size, itr_begin);
+    return true;
   }
+
 protected:
 };
 
---
+++ test/file_io_test.cpp
@@ -0,0 +1,111 @@
+#define BOOST_TEST_MODULE file io test
+
+#include "file_io.hpp"
+#include <boost/test/data/monomorphic.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/included/unit_test.hpp>
+#include <random>
+#include <stdexcept>
+
+// Anyhting more than 1MB is too large
+#define MAX_BUFFER_SIZE 1048576
+class reader_fixture {
+  std::string filename;
+  uint64_t block_size;
+  uint64_t block_count;
+  char *data_ptr;
+
+public:
+  reader_fixture(std::string filename, uint64_t block_size, uint64_t block_count)
+      : filename(filename),
+        block_size(block_size),
+        block_count(block_count),
+        data_ptr(nullptr) {
+    std::stringstream ss;
+    // PLATFORM_NOTE
+    ss << "dd status=none bs=" << this->block_size << " count=" << this->block_count
+       << " if=/dev/urandom of=" << this->filename << " > /dev/null 2>&1";
+
+    // ss.str("");
+    // ss << "echo 1234567890123456 > " << this->filename;
+    std::system(ss.str().c_str());
+  }
+
+  std::unique_ptr<char[]> get() {
+    uint64_t file_size = this->block_size * this->block_count;
+    std::unique_ptr<char[]> data_buffer(new char[file_size]);
+    if (file_size > MAX_BUFFER_SIZE) {
+      throw std::runtime_error("Too large file to read");
+    }
+    std::fstream handle(this->filename, std::ios::in | std::ios::binary);
+    handle.read(data_buffer.get(), file_size);
+    return data_buffer;
+  }
+
+  ~reader_fixture() {
+    std::stringstream ss;
+    // PLATFORM_NOTE
+    ss << "rm -rf " << this->filename;
+    std::system(ss.str().c_str());
+    if (this->data_ptr) {
+      delete[] this->data_ptr;
+    }
+  }
+};
+
+namespace bdata = boost::unit_test::data;
+BOOST_DATA_TEST_CASE(file_io_general_test,
+                     bdata::make({512, 1024, 1023, 2040}) ^ bdata::make(1, 20, 31, 12),
+                     block_size,
+                     block_count) {
+  std::stringstream ss;
+  ss << block_size << "_" << block_count;
+  std::string filename = ss.str();
+  auto r_fixture       = reader_fixture(filename, block_size, block_count);
+  auto data_ptr        = r_fixture.get();
+  fileio::reader sample_reader(filename);
+  BOOST_TEST(sample_reader.is_open(), "Failed to open file");
+  std::vector<char> sample_data;
+  std::streamsize bytes_read, read_size, total_read = 0;
+  size_t index = 0;
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<std::mt19937::result_type> dist(1, 2 * block_size);
+  read_size = dist(rng);
+  sample_data.resize(read_size);
+  while (sample_reader.fill_buffer(sample_data.begin(), sample_data.end(), bytes_read)) {
+    auto count = std::min(sample_data.end() - sample_data.begin(), bytes_read);
+    std::stringstream ss;
+    ss << "Wanted " << read_size << ", Read :" << bytes_read;
+    BOOST_TEST_MESSAGE(ss.str());
+    total_read += bytes_read;
+    if (std::equal(sample_data.begin(), sample_data.begin() + count, data_ptr.get() + index)) {
+      index += bytes_read;
+    } else {
+      BOOST_TEST(false, "Data mismatch");
+      break;
+    }
+    if (bytes_read != read_size) {
+      break;
+    }
+    read_size = dist(rng);
+    sample_data.resize(read_size);
+  }
+  BOOST_TEST(total_read == block_size * block_count, "Number of bytes read is not as much as expected");
+}
+
+BOOST_AUTO_TEST_CASE(file_io_read_non_existing_file) {
+  std::string non_existing_file = "file_io_test_random_file";
+  {
+    // This basically makes sure that this file gets delted if exists. It creates and "deletes" a file
+    auto r_fixture = reader_fixture(non_existing_file, 1, 1);
+    (void)(r_fixture);
+  }
+  fileio::reader sample_reader(non_existing_file);
+  BOOST_TEST(sample_reader.is_open() == false, "Non existing file is said as open");
+  std::array<char, 16> buffer;
+  std::streamsize bytes_read = 0;
+  BOOST_TEST(sample_reader.fill_buffer(buffer.begin(), buffer.end(), bytes_read) == false,
+             "Buffer was filled for non reader associated with non existing file");
+  BOOST_TEST(bytes_read == 0, "Numer of bytes read is non zero");
+}
