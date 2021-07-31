8a2dbec0c856234b6cdf5660499961567acb03ca
vikash chandola <vikashchandola26@gmail.com>
2021-07-31 20:41:57 +0530

    Updates upload download test
    Upload download test download is updated to runn operations in
    parllel. Test also checks whether files match.

---
+++ test/bin/frag_checker.sh
@@ -0,0 +1,16 @@
+echo "echo Comparing files..."
+cd ./server_dir/
+for i in sample_*
+do
+  cmp $i client_$i
+  if [[ $? -gt 0 ]]
+  then
+    echo "Mismatch in file $i"
+    exit 1
+  fi
+done
+for i in sample_*
+do
+  rm $i client_$i
+done
+cd -
