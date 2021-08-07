6865b3287de20bfeba3e5c0e4f482ec0e6ebcb87
vikash chandola <vikashchandola26@gmail.com>
2021-08-07 19:00:42 +0530

    Updates tftp frame unit tests

--- test/tftp_frame_test.cpp
+++ test/tftp_frame_test.cpp
@@ -8,50 +8,6 @@ void print_frame(const tftp::frame &f) {
   }
   std::cout << std::dec << std::endl;
 }
-int main() {
-  tftp::frame f;
-
-  std::cout << "Creating read request frame" << std::endl;
-  //f.reset();
-  f.set_option("ABCDEFGHIJKLMNOPQRSTUVWYZ0", "ABCDEFGHIJKLMNOPQRSTUVWYZ0");
-  /*f.set_option("ABCDEFGHIJKLMNOPQRSTUVWYZA", "ABCDEFGHIJKLMNOPQRSTUVWYZA");
-  f.set_option("ABCDEFGHIJKLMNOPQRSTUVWYZB", "ABCDEFGHIJKLMNOPQRSTUVWYZB");
-  f.set_option("ABCDEFGHIJKLMNOPQRSTUVWYZC", "ABCDEFGHIJKLMNOPQRSTUVWYZC");
-  f.set_option("ABCDEFGHIJKLMNOPQRSTUVWYZD", "ABCDEFGHIJKLMNOPQRSTUVWYZD");
-  f.set_option("ABCDEFGHIJKLMNOPQRSTUVWYZE", "ABCDEFGHIJKLMNOPQRSTUVWYZE");
-  f.set_option("ABCDEFGHIJKLMNOPQRSTUVWYZF", "ABCDEFGHIJKLMNOPQRSTUVWYZF");
-  f.set_option("ABCDEFGHIJKLMNOPQRSTUVWYZG", "ABCDEFGHIJKLMNOPQRSTUVWYZG");
-  f.set_option("ABCDEFGHIJKLMNOPQRSTUVWYZH", "ABCDEFGHIJKLMNOPQRSTUVWYZH");
-  f.set_option("ABCDEFGHIJKLMNOPQRSTUVWYZJ", "ABCDEFGHIJKLMNOPQRSTUVWYZJ");*/
-
-  f.make_read_request_frame("somefile");
-  print_frame(f);
-
-  std::cout << "Creating write request frame" << std::endl;
-  //f.reset();
-  f.make_write_request_frame("somefile");
-  print_frame(f);
-
-  std::cout << "Creating write request frame" << std::endl;
-  //f.reset();
-  f.make_write_request_frame("somefile");
-  print_frame(f);
-
-  std::cout << "Creating ack frame" << std::endl;
-  //f.reset();
-  f.make_ack_frame(15);
-  print_frame(f);
-
-  std::string s("123456789");
-  std::cout << "Creating data frame" << std::endl;
-  //f.reset();
-  f.make_data_frame(s.cbegin(), s.cend(), 15);
-  print_frame(f);
-
-  std::cout << "Done" << std::endl;
-  return 0;
-}
-/*
 #define BOOST_TEST_MODULE tftp frame
 
 #include <algorithm>
@@ -98,12 +54,12 @@ BOOST_DATA_TEST_CASE(read_write_request_frame_creation,
                      test_sample) {
   std::string filename         = test_sample.first;
   tftp::frame::op_code op_code = test_sample.second;
-  tftp::frame_s f;
+  tftp::frame f;
   try {
     if (op_code == tftp::frame::op_read_request) {
-      f = tftp::frame::create_read_request_frame(filename);
+      f.make_read_request_frame(filename);
     } else {
-      f->make_write_request_frame(filename);
+      f.make_write_request_frame(filename);
     }
   } catch (tftp::invalid_frame_parameter_exception &e) {
     if (filename.length() == 0 || filename.length() > 255) {
@@ -116,18 +72,15 @@ BOOST_DATA_TEST_CASE(read_write_request_frame_creation,
   if (filename.length() == 0 || filename.length() > 255) {
     BOOST_TEST(false);
   }
-
-  const std::vector<char> &f_data = f->get_frame_as_vector();
-  auto itr                        = f_data.cbegin();
+  auto itr = f.cbegin();
 
   BOOST_TEST(*itr == 0x00, "First byte of frame is not 0x00");
 
   itr++;
-  BOOST_TEST(f->get_op_code() == op_code, "OP Code mismatch");
+  BOOST_TEST(f.get_op_code() == op_code, "OP Code mismatch");
   BOOST_TEST(*itr == op_code, "OP Code mismatch");
 
   itr++;
-
   for (char ch : filename) {
     BOOST_TEST(*itr == ch, "filename mismatch in tftp frame");
     itr++;
@@ -135,7 +88,7 @@ BOOST_DATA_TEST_CASE(read_write_request_frame_creation,
 
   BOOST_TEST(*itr == 0x00, "Separator is not 0x00");
   itr++;
-  BOOST_TEST(f->get_data_mode() == tftp::frame::mode_octet, "Data mode is invalid");
+  BOOST_TEST(f.get_data_mode() == tftp::frame::mode_octet, "Data mode is invalid");
   for (char ch : std::string("octet")) {
     BOOST_TEST(*itr == ch, "Data mode is invalid");
     itr++;
@@ -144,7 +97,7 @@ BOOST_DATA_TEST_CASE(read_write_request_frame_creation,
   BOOST_TEST(*itr == 0x00, "Frame is not ending with 0x00");
 
   itr++;
-  BOOST_TEST((itr == f_data.cend()), "Frame is longer than expected");
+  BOOST_TEST((itr == f.cend()), "Frame is longer than expected");
 }
 
 BOOST_DATA_TEST_CASE(data_frame_creation, bdata::xrange(0, 600, 50), data_length) {
@@ -159,50 +112,48 @@ BOOST_DATA_TEST_CASE(data_frame_creation, bdata::xrange(0, 600, 50), data_length
   for (int i = 0; i < data_length; ++i) {
     data.push_back(static_cast<char>(distrib(gen)));
   }
-  tftp::frame_s f                 = tftp::frame::create_data_frame(data.cbegin(), data.cend(), block_number);
-  const std::vector<char> &f_data = f->get_frame_as_vector();
-  BOOST_TEST(f_data.size() >= 4, "Data frame is smaller than 4 bytes");
+  tftp::frame f;
+  f.make_data_frame(data.cbegin(), data.cend(), block_number);
+  BOOST_TEST((f.cend() - f.cbegin() >= 4), "Data frame is smaller than 4 bytes");
 
-  auto itr = f_data.cbegin();
-  BOOST_TEST(*itr == 0x00, "First byte of data frame is not 0x00");
+  auto itr = f.cbegin();
+  BOOST_TEST((*itr == 0x00), "First byte of data frame is not 0x00");
   itr++;
-  BOOST_TEST(*itr == tftp::frame::op_data, "Incorrect op code in data frame");
+  BOOST_TEST((*itr == tftp::frame::op_data), "Incorrect op code in data frame");
 
   itr++;
-  BOOST_TEST(f->get_block_number() == block_number, "Data frame block number mismatch");
-  BOOST_TEST(*itr == static_cast<char>(block_number >> 8), "Data frame block number mismatch");
+  BOOST_TEST((f.get_block_number() == block_number), "Data frame block number mismatch");
+  BOOST_TEST((*itr == static_cast<char>(block_number >> 8)), "Data frame block number mismatch");
   itr++;
   BOOST_TEST(*itr == static_cast<char>(block_number), "Data frame block number mismatch");
 
   itr++;
-  BOOST_TEST((f_data.cend() >= itr), "Invalid data in data frame");
+  BOOST_TEST((f.cend() >= itr), "Invalid data in data frame");
   for (std::size_t i = 0; i < std::min(data.size(), static_cast<std::size_t>(TFTP_FRAME_MAX_DATA_LEN)); i++) {
-    BOOST_TEST(*itr == data[i], "Data mismatch in data frame");
+    BOOST_TEST((*itr == data[i]), "Data mismatch in data frame");
     itr++;
   }
-  BOOST_TEST((itr == f_data.cend()), "Invalid end for data frame");
+  BOOST_TEST((itr == f.cend()), "Invalid end for data frame");
 }
 
 BOOST_DATA_TEST_CASE(ack_frame_creation, bdata::make({0, 1, 255, 1000, 65535}), block_number) {
-  tftp::frame_s f                 = tftp::frame::create_ack_frame(block_number);
-  const std::vector<char> &f_data = f->get_frame_as_vector();
-
-  auto itr                        = f_data.cbegin();
+  tftp::frame f;
+  f.make_ack_frame(block_number);
+  auto itr = f.cbegin();
   BOOST_TEST(*itr == 0x00, "First byte of data frame is not 0x00");
 
   itr++;
   BOOST_TEST(*itr == tftp::frame::op_ack, "Incorrect op code for ack frame");
 
   itr++;
-  BOOST_TEST(f->get_block_number() == block_number, "Data frame block number mismatch");
+  BOOST_TEST(f.get_block_number() == block_number, "Data frame block number mismatch");
   BOOST_TEST(*itr == static_cast<char>(block_number >> 8), "Data frame block number mismatch");
   itr++;
   BOOST_TEST(*itr == static_cast<char>(block_number), "Data frame block number mismatch");
 
   itr++;
-  BOOST_TEST((itr == f_data.cend()), "Invalid end for ack frame");
+  BOOST_TEST((itr == f.cend()), "Invalid end for ack frame");
 }
-
 std::map<tftp::frame::error_code, std::string> error_frame_dataset() {
   std::map<tftp::frame::error_code, std::string> dataset;
   for (uint32_t i = 0; i < 8; i++) {
@@ -219,23 +170,21 @@ BOOST_DATA_TEST_CASE(error_frame_creation, bdata::make(error_frame_dataset()), t
   std::string error_message          = test_sample.second;
   tftp::frame::error_code error_code = test_sample.first;
 
-  tftp::frame_s f;
+  tftp::frame f;
   if (error_message.empty()) {
-    f = tftp::frame::create_error_frame(error_code);
+    f.make_error_frame(error_code);
   } else {
-    f = tftp::frame::create_error_frame(error_code, error_message);
+    f.make_error_frame(error_code, error_message);
   }
 
-  const std::vector<char> &f_data = f->get_frame_as_vector();
-
-  auto itr                        = f_data.cbegin();
+  auto itr = f.cbegin();
   BOOST_TEST(*itr == 0x00, "First byte of data frame is not 0x00");
 
   itr++;
   BOOST_TEST(*itr == tftp::frame::op_error, "Incorrect op code for error frame");
 
   itr++;
-  BOOST_TEST((f->get_error_code() == error_code), "Error code mismatch");
+  BOOST_TEST((f.get_error_code() == error_code), "Error code mismatch");
   BOOST_TEST(*itr == static_cast<char>(static_cast<uint16_t>(error_code) >> 8), "Error code mismatch");
   itr++;
   BOOST_TEST(*itr == static_cast<char>(static_cast<uint16_t>(error_code)), "Error code mismatch");
@@ -248,8 +197,7 @@ BOOST_DATA_TEST_CASE(error_frame_creation, bdata::make(error_frame_dataset()), t
 
   itr++;
   if (!error_message.empty()) {
-    BOOST_TEST((itr == f_data.cend()), "Invalid end for ack frame");
+    BOOST_TEST((itr == f.cend()), "Invalid end for ack frame");
   }
-  BOOST_TEST(*(f_data.cend() - 1) == 0x00, "Last byte of ack frame is not 0x00");
+  BOOST_TEST(*(f.cend() - 1) == 0x00, "Last byte of ack frame is not 0x00");
 }
-*/
